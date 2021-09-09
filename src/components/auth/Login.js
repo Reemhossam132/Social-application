fc2f8097e566df29d394c4863cbc1e25454a5ae3
Reem <reemhossam132@gmail.com>
2021-09-09 13:20:32 +0200

    first commit

---
+++ src/components/auth/Login.js
@@ -0,0 +1,75 @@
+import React, { Fragment, useState } from "react";
+import { Link, Redirect } from "react-router-dom";
+import { connect } from "react-redux";
+import { login } from '../../actions/auth';
+import PropTypes from "prop-types";
+
+//import axios from 'axios'
+
+const Login = ({ login, isAuthenticated }) => {
+  const [formData, setFormData] = useState({
+    email: " ",
+    password: "",
+  });
+  const { email, password } = formData;
+
+  const onChange = (e) =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    login(email, password);
+  };
+
+  // redirect login
+
+  if (isAuthenticated) {
+    return <Redirect to="/dashboard" />;
+  }
+
+  return (
+    <Fragment>
+      <h1 class="large text-primary">Login</h1>
+      <p class="lead">
+        <i class="fas fa-user"></i> Create Your Account
+      </p>
+      <form class="form" onSubmit={(e) => onSubmit(e)}>
+        <div class="form-group">
+          <input
+            type="email"
+            placeholder="Email Address"
+            name="email"
+            value={email}
+            onChange={(e) => onChange(e)}
+        
+          />
+        </div>
+        <div class="form-group">
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+        
+            value={password}
+            onChange={(e) => onChange(e)}
+          />
+        </div>
+
+        <input type="submit" class="btn btn-primary" value="login" />
+      </form>
+      <p class="my-1">
+        Not have an account? <Link to="/register">Sign Up</Link>
+      </p>
+    </Fragment>
+  );
+};
+Login.propTypes = {
+  setAlert: PropTypes.func.isRequired,
+  login: PropTypes.func.isRequired,
+  isAuthenticated: PropTypes.bool,
+};
+
+const mapStateToProps = (state) => ({
+  isAuthenticated: state.auth.isAuthenticated,
+});
+export default connect(mapStateToProps, { login })(Login);
